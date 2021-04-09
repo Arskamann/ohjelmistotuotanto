e320aff3455a6a200d365c438e5872a231dde6e1
Arskamann <82201968+Arskamann@users.noreply.github.com>
2021-04-09 12:13:45 +0300

    Add files via upload

---
+++ Ohjelmistoo/src/softa/Menu.java
@@ -0,0 +1,64 @@
+package softa;
+
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Menu extends Application {
+
+    public void start(Stage alku) {
+    	  alku.setTitle("ListView Experiment 1");
+    	  Text x = new Text("");
+          ListView<String> listView = new ListView<String>();
+          Pane ff = new Pane();
+          ff.getChildren().add(x);
+         
+          listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+          listView.getItems().add("Uusi varaus");
+          listView.getItems().add("Varaukset ja muokkaus");
+          listView.getItems().add("Mökit");
+          listView.getItems().add("Toiminta-alueet");
+          listView.getItems().add("Laskutus");
+          listView.setPrefSize(100, 150);
+        
+          
+          listView.setOnMouseReleased((EventHandler<? super MouseEvent>) new EventHandler<Event>() { // tässä avataan sit eri näkymät ??? esimerkki vasta...
+
+              public void handle(Event event) {
+                  		x.setText("Valittuina: "+listView.getSelectionModel().getSelectedItems().toString().replace("]", "").replace("[",""));
+             
+                  
+
+              }
+
+          });
+
+         BorderPane b = new BorderPane();
+         b.setTop(listView);
+         b.setCenter(ff);
+         
+     
+     
+        
+
+        Scene kehys = new Scene(b,300,330);
+        alku.setTitle("Mökkivaraus softa !!!");
+        alku.setScene(kehys);
+        alku.show();
+    }
+    
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
