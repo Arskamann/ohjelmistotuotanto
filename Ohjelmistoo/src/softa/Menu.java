939621d10f986adc2d8ffda58b4ee4c8771b3c09
Arskamann <82201968+Arskamann@users.noreply.github.com>
2021-04-10 12:53:05 +0300

    alustus

--- Ohjelmistoo/src/softa/Menu.java
+++ Ohjelmistoo/src/softa/Menu.java
@@ -1,8 +1,16 @@
 package softa;
 
+import java.awt.Button;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
@@ -13,49 +21,10 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Menu extends Application {
+	
 
     public void start(Stage alku) {
-    	  alku.setTitle("ListView Experiment 1");
-    	  Text x = new Text("");
-          ListView<String> listView = new ListView<String>();
-          Pane ff = new Pane();
-          ff.getChildren().add(x);
-
-
-          //rejected?
-          listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-
-          listView.getItems().add("Uusi varaus");
-          listView.getItems().add("Varaukset ja muokkaus");
-          listView.getItems().add("Mökit");
-          listView.getItems().add("Toiminta-alueet");
-          listView.getItems().add("Laskutus");
-          listView.setPrefSize(100, 150);
-        
-          
-          listView.setOnMouseReleased((EventHandler<? super MouseEvent>) new EventHandler<Event>() { // tässä avataan sit eri näkymät ??? esimerkki vasta...
-
-              public void handle(Event event) {
-                  		x.setText("Valittuina: "+listView.getSelectionModel().getSelectedItems().toString().replace("]", "").replace("[",""));
-             
-                  
-
-              }
-
-          });
-
-         BorderPane b = new BorderPane();
-         b.setTop(listView);
-         b.setCenter(ff);
-         
-     
-     //testii wtf!!
-        
-
-        Scene kehys = new Scene(b,300,330);
-        alku.setTitle("Mökkivaraus softa !!!");
-        alku.setScene(kehys);
-        alku.show();
+    	
     }
     
 
