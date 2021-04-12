f16a99b69e2cb24b1f05dbd415158e62ef7f839e
Arskamann <82201968+Arskamann@users.noreply.github.com>
2021-04-12 12:24:30 +0300

    2 ikkunapohjaa nyt.. kysykää jos ette tajua jotakin

--- Ohjelmistoo/src/softa/Menu.java
+++ Ohjelmistoo/src/softa/Menu.java
@@ -23,14 +23,36 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Menu extends Application {
-	
-
+	private static Stage stg;
+	@FXML
+	Button nappi;
     public void start(Stage alku) throws IOException {
+    	stg=alku;
     	Parent root = FXMLLoader.load(getClass().getResource("Ikkuna.fxml"));
+    
     	alku.setTitle("Hieno softa");
     	alku.setScene(new Scene(root,300,300));
     	alku.show();
     }
+    
+    public void uusiVaraus(ActionEvent event) throws IOException {
+		changeScene("Uusivaraus.fxml");
+		
+		
+		
+	}
+    
+    
+    
+    
+    
+    
+    
+    public void changeScene(String fxml) throws IOException {                      // tällä vaihdetaan ikkuna esim napin painnalluksella
+    	Parent pane = FXMLLoader.load((getClass().getResource(fxml)));
+    	stg.getScene().setRoot(pane);
+    }
+    
     
 
     public static void main(String[] args) {
---
+++ Ohjelmistoo/src/softa/Uusivaraus.java
@@ -0,0 +1,20 @@
+package softa;
+
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+
+
+public class Uusivaraus extends Menu {
+
+	public void uusiVaraus(ActionEvent event) throws IOException {
+		changeScene("Uusivaraus.fxml");
+		
+		
+		
+	}
+	
+	
+	
+}
